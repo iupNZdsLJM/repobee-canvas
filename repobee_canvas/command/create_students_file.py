318375b5f434947123a2b520999a8d38868b63e0
Huub de Beer <h.t.d.beer@tue.nl>
2021-04-22 14:41:50 +0200

    Add check for groupless students when creating students list (#17)

--- repobee_canvas/command/create_students_file.py
+++ repobee_canvas/command/create_students_file.py
@@ -23,13 +23,13 @@ from ..canvas_git_map           import CanvasGitMap
 from .canvas_category          import CANVAS_CATEGORY
 
 from ..common_options           import CANVAS_ACCESS_TOKEN_OPTION
-from ..common_options           import CANVAS_COURSE_ID_OPTION
-from ..common_options           import CANVAS_ASSIGNMENT_ID_OPTION
-from ..common_options           import CANVAS_STUDENTS_FILE_OPTION
 from ..common_options           import CANVAS_API_BASE_URL_OPTION
+from ..common_options           import CANVAS_ASSIGNMENT_ID_OPTION
+from ..common_options           import CANVAS_COURSE_ID_OPTION
 from ..common_options           import CANVAS_GIT_MAP_OPTION
+from ..common_options           import CANVAS_STUDENTS_FILE_OPTION
 
-from ..tui                      import inform
+from ..tui                      import inform, warn
 
 class CreateStudentsFile(plug.Plugin, plug.cli.Command):
     """RepoBee command to create a students file from a Canvas assignment.
@@ -76,39 +76,100 @@ class CreateStudentsFile(plug.Plugin, plug.cli.Command):
                 )
             )
 
-    canvas_access_token     = CANVAS_ACCESS_TOKEN_OPTION
-    canvas_base_url         = CANVAS_API_BASE_URL_OPTION
-    canvas_course_id        = CANVAS_COURSE_ID_OPTION
-    canvas_assignment_id    = CANVAS_ASSIGNMENT_ID_OPTION
-    canvas_students_file    = CANVAS_STUDENTS_FILE_OPTION
-    canvas_git_map          = CANVAS_GIT_MAP_OPTION
+    canvas_access_token      = CANVAS_ACCESS_TOKEN_OPTION
+    canvas_base_url          = CANVAS_API_BASE_URL_OPTION
+    canvas_course_id         = CANVAS_COURSE_ID_OPTION
+    canvas_assignment_id     = CANVAS_ASSIGNMENT_ID_OPTION
+    canvas_students_file     = CANVAS_STUDENTS_FILE_OPTION
+    canvas_git_map           = CANVAS_GIT_MAP_OPTION
+
+    canvas_include_groupless = plug.cli.flag(
+        help = "Include students who are not assigned to a group for group assignments.",
+        )
 
     def command(self):
         CanvasAPI().setup(self.canvas_base_url, self.canvas_access_token)
         assignment = Assignment.load(self.canvas_course_id, self.canvas_assignment_id)
         canvas_git_mapping_table = CanvasGitMap.load(Path(self.canvas_git_map))
-        create_students_file(assignment, canvas_git_mapping_table, Path(self.canvas_students_file))
+        create_students_file(
+                assignment,
+                canvas_git_mapping_table,
+                Path(self.canvas_students_file),
+                self.canvas_include_groupless
+                )
         inform(f"Students file written to '{self.canvas_students_file}'.")
 
 def create_students_file(
         assignment                  : Assignment,
         canvas_git_mapping_table    : CanvasGitMap,
         students_file               : Path,
+        include_groupless_students  : bool = False
     ):
     """Create a students file for a Canvas assignment."""
+    include_groupless = not assignment.is_group_assignment() or include_groupless_students
+
+    submissions = assignment.submissions()
+    group_submissions = [s for s in submissions if s.is_group_submission()]
+
+    # Students who are not assigned to a group in a group assignment are
+    # ignored by default when creating a students file. Most likely these
+    # students are not actively participating in the course or have not yet
+    # assigned themselves to a group. However, with parameter
+    # include_groupless_students, you can override this default behavior.
+    if assignment.is_group_assignment():
+        if len(group_submissions) == 0:
+            raise ValueError(
+                ("No group submissions found for this group assignment. "
+                "Please run command 'repobee canvas prepare-assignment' to "
+                "resolve this issue. Or configure this assignment as an "
+                "individual assignment."))
+
+        if len(group_submissions) < len(submissions):
+            message = ((
+                f"The number of group submissions ({len(group_submissions)}) "
+                f"is smaller than the number of submissions "
+                f"({len(submissions)}). This means that not all students "
+                 "in this assignment are assigned to a group. "
+                ))
+
+            if include_groupless:
+                message += ((
+                    "These groupless students are included in "
+                    "the created students list file."
+                    ))
+            else:
+                message += ((
+                    "These groupless students are NOT included in the "
+                    "created students list file."
+                    "Use option '--canvas-include-groupless' to include "
+                    "these groupless students."
+                    ))
+
+            warn(message)
+
+
     with students_file.open("w") as outfile:
 
-        for submission in assignment.submissions():
+        for submission in submissions:
 
             if submission.is_group_submission():
+
                 group = [
                         canvas_git_mapping_table.canvas2git(u.login_id)
                         for u in submission.group().members()
                         ]
+
                 outfile.write(" ".join(group))
-            else:
+                outfile.write("\n")
+
+            elif include_groupless:
+                # Individual students
+
                 canvas_id   = submission.submitter().login_id
                 git_id      = canvas_git_mapping_table.canvas2git(canvas_id)
                 outfile.write(git_id)
+                outfile.write("\n")
 
-            outfile.write("\n")
+            else:
+                # Skip individual students in a group assignment
+                pass
